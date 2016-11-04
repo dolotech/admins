c3cf2532922a473913e2b81118dd8f450e304a9a
dolotech <dolotech@163.com>
2016-11-04 13:01:07 +0800

    add linux run shell

---
+++ BuildLinux.bat
@@ -0,0 +1,13 @@
+@echo Setting up environment for GOPATH from %~dp0.
+@set GOPATH=%~dp0
+
+
+@set GOPATH=%~dp0
+set GOARCH=amd64
+set GOOS=linux
+
+
+
+go build  -o ./bin/admins -ldflags "-w -s" ./src/admin.go
+
+pause
