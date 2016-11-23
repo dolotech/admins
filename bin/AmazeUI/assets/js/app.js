90687fcef84fc3ef2fa88399863d4805bdadae15
dolotech <dolotech@163.com>
2016-11-23 10:12:29 +0800

    conver headbar to vue component

--- bin/AmazeUI/assets/js/app.js
+++ bin/AmazeUI/assets/js/app.js
@@ -103,6 +103,28 @@ function post(URL, PARAMS) {
     return temp;
 }
 
+function getHeadBar() {
+    return [
+        {
+            name:  "管理员",
+            icon: "am-icon-users",
+            items:    [
+                {icon:"am-icon-edit",path:"/users/group_list.html", name:"用户组管理"},
+                {icon:"am-icon-edit",path:"/users/list.html", name:"后台用户账号管理"},
+                {icon:"am-icon-edit",path:"#", name:"后台操作日志"},
+                {icon:"am-icon-edit",path:"/users/edit.html", name:"修改我的密码"}]
+        },
+        {
+            name:  "系统设置",
+            icon: "am-icon-bars",
+            items:    [
+                {icon:"am-icon-user",path:"#", name:"资料"},
+                {icon:"am-icon-cog",path:"#", name:"设置"},
+                {icon:"am-icon-power-off",path:"#",action:"javascript:onlogout()", name:"退出"}]
+        }
+    ]
+}
+
 
 function getSideBar() {
     return [
@@ -151,42 +173,3 @@ function getSideBar() {
         }
     ]
 }
-
-var MyComponent = Vue.extend({
-    template : '<div class="admin-sidebar am-offcanvas" id="admin-offcanvas">'+
-    '  <div class="am-offcanvas-bar admin-offcanvas-bar">'+
-    '<ul class="am-list admin-sidebar-list">'+
-    ' <li><a href="admin-index.html"><span class="am-icon-home"></span> 首页</a></li>'+
-    ' <li class="admin-parent">'+
-    ' <a class="am-cf" data-am-collapse="{target: '+"'#collapse-nav'" +'}"><span class="am-icon-file"></span> 页面模块 <span class="am-icon-angle-right am-fr am-margin-right"></span></a>'+
-' <ul class="am-list am-collapse admin-sidebar-sub am-in" id="collapse-nav">'+
-' <li><a href="/roles/list.html" class="am-cf"><span class="am-icon-check"></span> 个人资料<span class="am-icon-star am-fr am-margin-right admin-icon-yellow"></span></a></li>'+
-'  <li><a href="/roles/listonline.html"><span class="am-icon-puzzle-piece"></span> 帮助页</a></li>'+
-'  <li><a href="admin-gallery.html"><span class="am-icon-th"></span> 相册页面<span class="am-badge am-badge-secondary am-margin-right am-fr">24</span></a></li>'+
- '<li><a href="admin-log.html"><span class="am-icon-calendar"></span> 系统日志</a></li>'+
-'   <li><a href="admin-404.html"><span class="am-icon-bug"></span> 404</a></li>'+
-'   </ul>'+
-'  </li>'+
-'   <li><a href="admin-table.html"><span class="am-icon-table"></span> 表格</a></li>'+
-'  <li><a href="admin-form.html"><span class="am-icon-pencil-square-o"></span> 表单</a></li>'+
-'  <li><a href="#"><span class="am-icon-sign-out"></span> 注销</a></li>'+
-'   </ul>'+
-
-'   <div class="am-panel am-panel-default admin-sidebar-panel">'+
-'   <div class="am-panel-bd">'+
-'  <p><span class="am-icon-bookmark"></span> 公告</p>'+
-'  <p>时光静好，与君语；细水流年，与君同。—— Amaze UI</p>'+
-'</div>'+
-'</div>'+
-
-'<div class="am-panel am-panel-default admin-sidebar-panel">'+
-'  <div class="am-panel-bd">'+
-' <p><span class="am-icon-tag"></span> wiki</p>'+
-' <p>Welcome to the Amaze UI wiki!</p>'+
-'</div>'+
-'</div>'+
-'</div>'+
-'</div>'
-})
-
-Vue.component('my-component',  MyComponent)
